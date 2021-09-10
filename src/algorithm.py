4893feed203de924023c84b4e908b5ed88eacc61
Mohammad R. Yousefi <myousefi@unm.edu>
2021-09-10 10:35:26 -0600

    Revert "Checkpoint"
    This reverts commit f880cbed15c213f6eb82ff26483053e868893c8c.

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,54 +1,44 @@
-import tensorflow as tf
-import numpy as np
-import time
-import multiprocessing
-
-
-class SimpleHyperParametersSpace:
-    def __init__(self, N, K, dtype=None, seed=None):
-        self.dims = N
-        self.choices = K
-        self.values = np.empty(shape=(self.dims, self.choices), dtype=dtype)
-        if seed is None:
-            self.seed = time.time_ns()
-            self.rng = np.random.default_rng(self.seed)
-        else:
-            self.rng = np.random.default_rng(seed)
-
-    def choose(self, dim, probabilities=None):
-        return self.rng.choice(self.values[dim], p=probabilities, shuffle=False)
-
-    def add_choices(self, dim, choices):
-        assert len(choices) == self.choices, 'Mismatch choice count'
-        self.values[dim] = choices
-
-    def add_values(self, values: np.ndarray):
-        assert values.dtype == self.values.dtype, 'Type mismatch'
-        assert values.shape == self.values.shape, 'Values must have the same dimensions'
-        self.values[:] = values
-
-
-LOCAL_PARAMS = None  # dict
-RL_PARAMS = None  # dict
+LOCAL_PARAMS = None # dict
+RL_PARAMS = None # dict
 
 BUFFER_SIZE = 50000
 BATCH_SIZE = 100
-HYPER_PARAM_SPACE = None  # Dictionary to be used in building networks.
+HYPER_PARAM_SPACE = None # Dictionary to be used in building networks.
 
-EVAL_METRICS = None  # List[Metric]
+EVAL_METRICS = None # List[Metric]
 
-TRAIN_DATA = None  # dict(np.array)
-EVAL_DATA = None  # dict(np.array)
-TEST_DATA = None  # dict(np.array)
+TRAIN_DATA = None # dict(np.array)
+EVAL_DATA = None # dict(np.array)
+TEST_DATA = None # dict(np.array)
+
+def normalize(values):
+    pass
+
+def evaluate(model, input_features, ground_truth, method, *metrics):
+    predictions = model.predict(input_features)
+    metric_values = []
+    for metric in metrics:
+        metric_values.append(metric.evaluate(predictions, ground_truth))
+
+    if method == 'mean_value':
+        return sum(metric_values) / len(metric_values)
+    elif method == 'mean_norm':
+        norm_values = sum(map(lambda x: normalize(x), metric_values)) / len(metric_values)
+
+def build_rn_network(space):
+    # Generate a network which returns hyper parameters 1 at a time.
 
 
-def build_rnn_network(space: SimpleHyperParametersSpace):
-    model = tf.keras.Sequential()
-    model.add(tf.keras.Input((space.choices,), name='OneHotInput'))
-    model.add(tf.keras.layers.LSTM(128), name='LSTM')
-    model.add(tf.keras.layers.Dense(space.choices, activation='softmax', name='Probabilities'))
-    return model
 
+def build_network(actions, values_dict):
+    # Generate a network for local search using parameters from a provided 
+    # hyper-parameter vector. Legality of each action is determined by 
+    # translation of the next vector value into an index which is applied
+    # to the next values dict.
+    # General loop
+    # Start Model
+    # For each action_value in actions -> use values_dict[PARAMETER_TYPE][action_value]
+    pass
 
 def generate_new_vector(rn_network):
     # actions = [next_prediction(rn_network)]
@@ -57,22 +47,21 @@ def generate_new_vector(rn_network):
     # return actions
     pass
 
-
 class Buffer:
     def __init__(self, size, batch_size):
         self.batch_size = batch_size
         self.max_size = size
         self.buffer_loc = 0
         self.record_index = 0
-        self.buffer = np.empty(shape=(self.max_size,), dtype=object)
+        self.buffer = numpy.empty(shape=(self.max_size, ), dtype=object)
         self.is_locked = False
 
-    def next_batch(self):
+    def next_batch():
         batch = self.buffer[self.buffer_loc: self.buffer_loc + self.batch_size]
         self.buffer_loc += self.batch_size
         return batch
 
-    def record(self, values):
+    def record(values):
         if self.is_locked:
             raise BufferError('Operation invoked while in Read-Only Mode.')
         if self.record_index == self.max_size:
@@ -80,34 +69,36 @@ class Buffer:
         self.buffer[self.record_index] = values
         self.record_index += 1
 
-    def reset(self, unlock=False):
+    def reset(unlock=False):
         if unlock:
             self.unlock()
         self.buffer_loc = 0
         if not self.is_locked:
             self.record_index = 0
 
-    def lock(self):
+    def lock():
         self.is_locked = True
 
-    def unlock(self):
+    def unlock():
         self.is_locked = False
 
-    def is_ready(self):
+    def is_ready():
         return self.buffer_loc < self.record_index
 
-    def get_last(self):
+    def get_last():
         if self.record_index:
             return self.buffer[self.record_index - 1]
         else:
             raise BufferError('Buffer is empty.')
 
 
+
+
 def reinforcement_learning_outer_loop(space, reward_function, training_length):
     max_reward = -float('inf')
     max_model = None
 
-    rn_network = build_rnn_network(len(input_space))
+    rn_network = create_rn_network(len(input_space))
     actions = Buffer(BUFFER_SIZE, BATCH_SIZE)
     rewards = Buffer(BUFFER_SIZE, BATCH_SIZE)
     for i in range(training_length):
@@ -122,10 +113,12 @@ def reinforcement_learning_outer_loop(space, reward_function, training_length):
     return max_model
 
 
+
 def select_hp_vector(policy, space):
     pass
-
 
 def update(rn, action_batch, reward_batch):
     # Compute Nabla R with "Neural Architecture Search with Reinforcement Learning, arXiv:1611.01578v2"
     pass
+
+
--- src/distance.py
+++ src/distance.py
@@ -9,11 +9,9 @@ import scipy.ndimage as nd
 
 def parse_args(args):
     ap = argparse.ArgumentParser(
-        description='Computes the euclidean distance transform for the sparse boundary matrix in the given shape. '
-                    'The boundary matrix contains all voxels on the surface boundaries.')
+        description='Computes the euclidean distance transform for the sparse boundary matrix in the given shape. The boundary matrix contains all voxels on the surface boundaries.')
     ap.add_argument(dest='in_file', help='Path to 2D sparse matrix data file with shape (Samples, Values)',
                     metavar='InFile')
-    ap.add_argument('--skip', dest='skip', type=int, help='Skips the first N samples', default=0)
     ap.add_argument('-s', dest='shape', nargs='+', type=int, help='Volume Space Dimensions', metavar='Dimension')
     ap.add_argument('-f', dest='features', type=int, help='The number of features at the beginning of each record',
                     default=0, metavar='Features')
@@ -24,7 +22,7 @@ def parse_args(args):
 
     ns.in_file = pathlib.Path(ns.in_file)
     if ns.out_file is None:
-        ns.out_file = ns.in_file.with_name(ns.in_file.stem + '_edt').with_suffix('.npz')
+        ns.out_file = ns.in_file.withstem(ns.in_file.stem + '_edt')
     else:
         ns.out_file = ns.in_file.joinpath(ns.out_file)
         if ns.out_file.suffix != '.npz':
@@ -46,7 +44,7 @@ if __name__ == '__main__':
     if params.shape is None:
         raise ValueError('Shape undefined.')
     print('Loading data ...', end='')
-    mtx = sp.load_npz(params.in_file)[params.skip:, params.features:]
+    mtx = sp.load_npz(params.in_file)[:, params.features:]
     print(' done.\nComputing euclidean distances ...', end='')
     edt = compute_edt(mtx, params.shape)
     print(' done.\nSaving results ...', end='')
